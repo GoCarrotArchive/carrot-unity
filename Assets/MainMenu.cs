6adc507a68eeaec5a255a2859702f1bf1508b25a
Pat Wilson <zerostride@gmail.com>
2012-11-20 13:53:17 -0800

    Add a GUI menu to allow for easy testing of Carrot apps

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+   public int buttonHeight = 20;
+   public int buttonWidth = 200;
+   public int buttonSpacing = 8;
+
+   string scoreString = "100";
+   string achieveString = "achievement_id";
+   string authStatus = "";
+
+   void AuthenticationStatusChangedHandler(object sender, Carrot.AuthStatus status)
+   {
+      authStatus = Carrot.authStatusString(status);
+   }
+
+   void Start()
+   {
+      authStatus = Carrot.authStatusString(Carrot.AuthStatus.Undetermined);
+      Carrot.AuthenticationStatusChanged += AuthenticationStatusChangedHandler;
+   }
+
+   void OnGUI()
+   {
+      GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+
+      // Display auth status
+      GUILayout.Label(authStatus);
+
+      // Facebook auth
+      GUILayout.Space(buttonSpacing);
+      if(GUILayout.Button("Facebook SSO", GUILayout.Height(buttonHeight)))
+      {
+         if(!Carrot.Instance.doFacebookAuth())
+         {
+            Debug.Log("Facebook SSO did not start successfully.");
+         }
+      }
+
+      // High Score
+      GUILayout.Space(buttonSpacing);
+      scoreString = GUILayout.TextField(scoreString, buttonWidth);
+      if(GUILayout.Button("Earn High Score", GUILayout.Height(buttonHeight)))
+      {
+         Carrot.Instance.postHighScore(System.Convert.ToUInt32(scoreString));
+      }
+
+      // Achievement
+      GUILayout.Space(buttonSpacing);
+      achieveString = GUILayout.TextField(achieveString, buttonWidth);
+      if(GUILayout.Button("Earn Achievement", GUILayout.Height(buttonHeight)))
+      {
+         Carrot.Instance.postAchievement(achieveString);
+      }
+
+      GUILayout.EndArea();
+   }
+}
